d88304c2430282da46fef783e9c7722693c7e3ea
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-05-29 15:34:41 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,21 +2,60 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "27/05/2026",
-    "fecha_iso": "2026-05-27",
-    "turno": "1494747985014084-00001-20260527TB.001",
-    "apertura": "2026-05-27T12:48:46",
+    "fecha": "29/05/2026",
+    "fecha_iso": "2026-05-29",
+    "turno": "1494747985014084-00001-20260529TB.002",
+    "apertura": "2026-05-29T16:47:25",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 48240.0,
+    "total_neto": 40537.0,
+    "total_iva": 7703.0,
+    "n_ordenes": 2,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "tarjeta": 48240.0,
+    "productos_top": [
+      {
+        "nombre": "Vegetariana",
+        "cantidad": 2,
+        "total": 19580.0
+      },
+      {
+        "nombre": "Margherita",
+        "cantidad": 1,
+        "total": 9590.0
+      },
+      {
+        "nombre": "Blu",
+        "cantidad": 1,
+        "total": 8390.0
+      },
+      {
+        "nombre": "Gnocchi Alfredo",
+        "cantidad": 1,
+        "total": 6490.0
+      },
+      {
+        "nombre": "Jugo natural 350 cc",
+        "cantidad": 1,
+        "total": 2300.0
+      },
+      {
+        "nombre": "Néctar 300 cc",
+        "cantidad": 1,
+        "total": 1890.0
+      }
+    ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-05-29",
+      "turno": "1494747985014084-00001-20260529TB.002"
+    },
+    {
+      "fecha": "2026-05-28",
+      "turno": "1494747985014084-00001-20260528TB.002"
+    },
     {
       "fecha": "2026-05-27",
       "turno": "1494747985014084-00001-20260527TB.001"
@@ -40,20 +79,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-05-20",
       "turno": "1494747985014084-00001-20260520TB.002"
-    },
-    {
-      "fecha": "2026-05-19",
-      "turno": "1494747985014084-00001-20260519TB.002"
-    },
-    {
-      "fecha": "2026-05-18",
-      "turno": "1494747985014084-00001-20260518TB.001"
-    },
-    {
-      "fecha": "2026-05-16",
-      "turno": "1494747985014084-00001-20260516TB.002"
     }
   ],
-  "total_cierres_historicos": 54,
-  "actualizadoEl": "27/05/2026 09:14"
+  "total_cierres_historicos": 58,
+  "actualizadoEl": "29/05/2026 15:25"
 };
