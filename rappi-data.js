787ffc19b49a2b9371a2fc7dbffa2cf87fc0f1b1
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-06-01 21:20:58 -0400

    Actualizar rappi-data.js

--- rappi-data.js
+++ rappi-data.js
@@ -5,18 +5,59 @@ window.RAPPI_DATA = {
   "is_enabled": false,
   "store_tier": "standard",
   "ventas_7d": {
-    "total_amount": 0,
-    "total_orders": 0,
-    "orders_avg": 0,
-    "users_count": 0,
-    "new_users_count": 0,
-    "last_week": []
+    "total_amount": 31130.0,
+    "total_orders": 2,
+    "orders_avg": 15565.0,
+    "users_count": 2,
+    "new_users_count": 2,
+    "last_week": [
+      {
+        "date": "2026-05-24",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-05-25",
+        "amount": 3110.0,
+        "orders": 1
+      },
+      {
+        "date": "2026-05-26",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-05-27",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-05-28",
+        "amount": 28020.0,
+        "orders": 1
+      },
+      {
+        "date": "2026-05-29",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-05-30",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-05-31",
+        "amount": 0,
+        "orders": 0
+      }
+    ]
   },
   "indicadores": {
     "cancellation_pct": 0,
     "error_pct": 0,
-    "availability_pct": 0,
+    "availability_pct": 60.18,
     "cooking_time_avg": 0
   },
-  "actualizadoEl": "01/06/2026 16:20"
+  "actualizadoEl": "01/06/2026 21:16"
 };
