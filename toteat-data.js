5fb56a78be8b63810b820c2a19b21c2c7cf5654d
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-06-16 08:37:05 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,72 +2,29 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "13/06/2026",
-    "fecha_iso": "2026-06-13",
-    "turno": "1494747985014084-00001-20260613TB.001",
-    "apertura": "",
+    "fecha": "16/06/2026",
+    "fecha_iso": "2026-06-16",
+    "turno": "1494747985014084-00001-20260616TB.001",
+    "apertura": "2026-06-16T12:30:36",
     "cierre": "",
-    "total_ventas": 173960.0,
-    "total_neto": 197292.0,
-    "total_iva": 37488.0,
-    "n_ordenes": 15,
+    "total_ventas": 0,
+    "total_neto": 0,
+    "total_iva": 0,
+    "n_ordenes": 0,
     "n_anuladas": 0,
-    "efectivo": 11200.0,
-    "tarjeta": 177277.0,
-    "productos_top": [
-      {
-        "nombre": "Promo completo italiano",
-        "cantidad": 11,
-        "total": 32890.0
-      },
-      {
-        "nombre": "Mechada queso y palta",
-        "cantidad": 2,
-        "total": 17500.0
-      },
-      {
-        "nombre": "Quattro Formaggi",
-        "cantidad": 2,
-        "total": 16780.0
-      },
-      {
-        "nombre": "Tradición de la casa",
-        "cantidad": 3,
-        "total": 16500.0
-      },
-      {
-        "nombre": "Menú ejecutivo",
-        "cantidad": 2,
-        "total": 14000.0
-      },
-      {
-        "nombre": "Jugo natural 350 cc",
-        "cantidad": 5,
-        "total": 11500.0
-      },
-      {
-        "nombre": "Vegetariana",
-        "cantidad": 1,
-        "total": 8390.0
-      },
-      {
-        "nombre": "Mechada",
-        "cantidad": 1,
-        "total": 7990.0
-      },
-      {
-        "nombre": "Bebida 350 cc",
-        "cantidad": 4,
-        "total": 7560.0
-      },
-      {
-        "nombre": "Margherita",
-        "cantidad": 1,
-        "total": 7190.0
-      }
-    ]
+    "efectivo": 0,
+    "tarjeta": 0,
+    "productos_top": []
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-06-16",
+      "turno": "1494747985014084-00001-20260616TB.001"
+    },
+    {
+      "fecha": "2026-06-15",
+      "turno": "1494747985014084-00001-20260615TB.001"
+    },
     {
       "fecha": "2026-06-13",
       "turno": "1494747985014084-00001-20260613TB.001"
@@ -99,12 +56,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-06-05",
       "turno": "1494747985014084-00001-20260605TB.002"
-    },
-    {
-      "fecha": "2026-06-04",
-      "turno": "1494747985014084-00001-20260604TB.002"
     }
   ],
-  "total_cierres_historicos": 79,
-  "actualizadoEl": "15/06/2026 08:59"
+  "total_cierres_historicos": 81,
+  "actualizadoEl": "16/06/2026 08:31"
 };
