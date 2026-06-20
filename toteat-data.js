19827d1bd9e858f5d036e93bb201ff0d6c4d934a
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-06-19 23:29:21 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,24 +2,87 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "16/06/2026",
-    "fecha_iso": "2026-06-16",
-    "turno": "1494747985014084-00001-20260616TB.001",
-    "apertura": "2026-06-16T12:30:36",
+    "fecha": "19/06/2026",
+    "fecha_iso": "2026-06-19",
+    "turno": "1494747985014084-00001-20260619TB.001",
+    "apertura": "2026-06-19T12:17:25",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 123210.0,
+    "total_neto": 148363.0,
+    "total_iva": 28187.0,
+    "n_ordenes": 10,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "tarjeta": 134572.0,
+    "productos_top": [
+      {
+        "nombre": "Menú ejecutivo",
+        "cantidad": 8,
+        "total": 56000.0
+      },
+      {
+        "nombre": "Fettuccini con salsa",
+        "cantidad": 3,
+        "total": 14970.0
+      },
+      {
+        "nombre": "Pollo Barbecue",
+        "cantidad": 1,
+        "total": 9590.0
+      },
+      {
+        "nombre": "Bebida 350 cc",
+        "cantidad": 5,
+        "total": 9450.0
+      },
+      {
+        "nombre": "Pepperoni",
+        "cantidad": 1,
+        "total": 8390.0
+      },
+      {
+        "nombre": "Pollo queso y palta",
+        "cantidad": 1,
+        "total": 6750.0
+      },
+      {
+        "nombre": "As italiano",
+        "cantidad": 1,
+        "total": 5990.0
+      },
+      {
+        "nombre": "Promo completo italiano",
+        "cantidad": 2,
+        "total": 5980.0
+      },
+      {
+        "nombre": "Completo Italiano",
+        "cantidad": 1,
+        "total": 4490.0
+      },
+      {
+        "nombre": "Té variedades",
+        "cantidad": 1,
+        "total": 1600.0
+      }
+    ]
   },
   "historial_cierres": [
     {
+      "fecha": "2026-06-19",
+      "turno": "1494747985014084-00001-20260619TB.001"
+    },
+    {
+      "fecha": "2026-06-18",
+      "turno": "1494747985014084-00001-20260618TB.002"
+    },
+    {
+      "fecha": "2026-06-17",
+      "turno": "1494747985014084-00001-20260617TB.002"
+    },
+    {
       "fecha": "2026-06-16",
-      "turno": "1494747985014084-00001-20260616TB.001"
+      "turno": "1494747985014084-00001-20260616TB.002"
     },
     {
       "fecha": "2026-06-15",
@@ -44,20 +107,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-06-09",
       "turno": "1494747985014084-00001-20260609TB.002"
-    },
-    {
-      "fecha": "2026-06-08",
-      "turno": "1494747985014084-00001-20260608TB.002"
-    },
-    {
-      "fecha": "2026-06-06",
-      "turno": "1494747985014084-00001-20260606TB.002"
-    },
-    {
-      "fecha": "2026-06-05",
-      "turno": "1494747985014084-00001-20260605TB.002"
     }
   ],
-  "total_cierres_historicos": 81,
-  "actualizadoEl": "16/06/2026 08:31"
+  "total_cierres_historicos": 87,
+  "actualizadoEl": "19/06/2026 23:20"
 };
