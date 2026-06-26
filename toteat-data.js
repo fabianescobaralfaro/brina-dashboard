551ffb0ca5ec2ecc9856157c45b1976898c55405
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-06-26 12:22:05 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,24 +2,63 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "23/06/2026",
-    "fecha_iso": "2026-06-23",
-    "turno": "1494747985014084-00001-20260623TB.001",
-    "apertura": "2026-06-23T13:58:02",
+    "fecha": "25/06/2026",
+    "fecha_iso": "2026-06-25",
+    "turno": "1494747985014084-00001-20260625TB.001",
+    "apertura": "",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 84570.0,
+    "total_neto": 98462.0,
+    "total_iva": 18708.0,
+    "n_ordenes": 9,
     "n_anuladas": 0,
-    "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "efectivo": 20940.0,
+    "tarjeta": 69993.0,
+    "productos_top": [
+      {
+        "nombre": "Menú ejecutivo",
+        "cantidad": 8,
+        "total": 56000.0
+      },
+      {
+        "nombre": "Vegetariana",
+        "cantidad": 1,
+        "total": 8390.0
+      },
+      {
+        "nombre": "1/4 pizza y bebida",
+        "cantidad": 2,
+        "total": 7900.0
+      },
+      {
+        "nombre": "Jugo natural 350 cc",
+        "cantidad": 3,
+        "total": 6900.0
+      },
+      {
+        "nombre": "Promo completo italiano",
+        "cantidad": 1,
+        "total": 2990.0
+      },
+      {
+        "nombre": "1/4 pizza",
+        "cantidad": 1,
+        "total": 2390.0
+      }
+    ]
   },
   "historial_cierres": [
     {
+      "fecha": "2026-06-25",
+      "turno": "1494747985014084-00001-20260625TB.001"
+    },
+    {
+      "fecha": "2026-06-24",
+      "turno": "1494747985014084-00001-20260624TB.002"
+    },
+    {
       "fecha": "2026-06-23",
-      "turno": "1494747985014084-00001-20260623TB.001"
+      "turno": "1494747985014084-00001-20260623TB.002"
     },
     {
       "fecha": "2026-06-22",
@@ -44,20 +83,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-06-16",
       "turno": "1494747985014084-00001-20260616TB.002"
-    },
-    {
-      "fecha": "2026-06-15",
-      "turno": "1494747985014084-00001-20260615TB.001"
-    },
-    {
-      "fecha": "2026-06-13",
-      "turno": "1494747985014084-00001-20260613TB.001"
-    },
-    {
-      "fecha": "2026-06-12",
-      "turno": "1494747985014084-00001-20260612TB.002"
     }
   ],
-  "total_cierres_historicos": 91,
-  "actualizadoEl": "23/06/2026 12:14"
+  "total_cierres_historicos": 95,
+  "actualizadoEl": "26/06/2026 07:18"
 };
