b7e1e8541c11991894427882d8fb4b75126a6561
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-07-06 00:02:18 -0400

    Actualizar haulmer-data.js

--- haulmer-data.js
+++ haulmer-data.js
@@ -1,6 +1,6 @@
 /* Generado automáticamente por scraper.py — NO editar manualmente */
 window.HAULMER_DATA = {
-  "actualizadoEl": "05/07/2026 22:49",
+  "actualizadoEl": "06/07/2026 00:00",
   "error": null,
   "informe_dia": {
     "periodo": "Día",
@@ -12,7 +12,16 @@ window.HAULMER_DATA = {
     "debito": 0,
     "credito": 0,
     "efectivo": 0,
-    "pos_breakdown": [{"serial": "6010B232511900275", "total_ventas": 0, "debito": 0, "credito": 0, "efectivo": 0, "propinas": 0}, {"serial": "PB1T238420182", "total_ventas": 0, "debito": 0, "credito": 0, "efectivo": 0, "propinas": 0}],
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 0,
+        "debito": 0,
+        "credito": 0,
+        "efectivo": 0,
+        "propinas": 0
+      }
+    ],
     "n_tx_debito": 0,
     "n_tx_credito": 0,
     "n_tx_efectivo": 0,
@@ -20,36 +29,28 @@ window.HAULMER_DATA = {
   },
   "informe_semana": {
     "periodo": "Semana",
-    "monto_neto": 1515073,
-    "iva": 287887,
-    "monto_ventas": 1802960,
-    "monto_exento": 1802960,
-    "propinas": 74057,
-    "debito": 1265951,
-    "credito": 87130,
-    "efectivo": 523940,
+    "monto_neto": 0,
+    "iva": 0,
+    "monto_ventas": 0,
+    "monto_exento": 0,
+    "propinas": 0,
+    "debito": 0,
+    "credito": 0,
+    "efectivo": 0,
     "pos_breakdown": [
       {
         "serial": "PB1T238420182",
-        "total_ventas": 985420,
-        "debito": 535680,
-        "credito": 59600,
-        "efectivo": 390140,
-        "propinas": 0
-      },
-      {
-        "serial": "6010B232511900275",
-        "total_ventas": 891601,
-        "debito": 730271,
-        "credito": 27530,
-        "efectivo": 133800,
+        "total_ventas": 0,
+        "debito": 0,
+        "credito": 0,
+        "efectivo": 0,
         "propinas": 0
       }
     ],
-    "n_tx_debito": 271,
-    "n_tx_credito": 21,
-    "n_tx_efectivo": 214,
-    "n_tx_total": 506
+    "n_tx_debito": 0,
+    "n_tx_credito": 0,
+    "n_tx_efectivo": 0,
+    "n_tx_total": 0
   },
   "informe_mes": {
     "periodo": "Mes",
@@ -85,13 +86,194 @@ window.HAULMER_DATA = {
     "n_tx_total": 393
   },
   "dte": {
-    "total_declarado": 0,
-    "total": 0,
-    "boletas": 0,
+    "total_declarado": 30,
+    "total": 30,
+    "boletas": 30,
     "facturas": 0,
-    "monto_boletas": 0,
+    "monto_boletas": 66190,
     "monto_facturas": 0,
-    "documentos": [],
+    "documentos": [
+      {
+        "tipo": "Boleta",
+        "numero": "35902",
+        "monto": 3650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35901",
+        "monto": 2150,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35900",
+        "monto": 3150,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35899",
+        "monto": 1000,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35898",
+        "monto": 1650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35897",
+        "monto": 2000,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35896",
+        "monto": 5900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35895",
+        "monto": 6650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35894",
+        "monto": 2900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35893",
+        "monto": 1650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35892",
+        "monto": 650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35891",
+        "monto": 2650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35890",
+        "monto": 6900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35889",
+        "monto": 6150,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35888",
+        "monto": 2290,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35887",
+        "monto": 1900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35886",
+        "monto": 400,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35885",
+        "monto": 2900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35884",
+        "monto": 1400,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35883",
+        "monto": 1300,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35882",
+        "monto": 1900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35881",
+        "monto": 1650,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35880",
+        "monto": 400,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35879",
+        "monto": 400,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35878",
+        "monto": 1000,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35877",
+        "monto": 400,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35876",
+        "monto": 1000,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35875",
+        "monto": 900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35874",
+        "monto": 900,
+        "fecha": ""
+      },
+      {
+        "tipo": "Boleta",
+        "numero": "35873",
+        "monto": 400,
+        "fecha": ""
+      }
+    ],
     "paginas_leidas": 1,
     "nota": ""
   },
@@ -105,8 +287,109 @@ window.HAULMER_DATA = {
     "n_transacciones_hoy": 0
   },
   "ventas_hoy": {
-    "total_dia": 0,
-    "transacciones": []
+    "total_dia": 81320,
+    "transacciones": [
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 5122",
+        "monto": 3150
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 1818",
+        "monto": 1650
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35902",
+        "monto": 3650
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 1844",
+        "monto": 2400
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 0471",
+        "monto": 10990
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 0471",
+        "monto": 2150
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35901",
+        "monto": 2150
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35900",
+        "monto": 3150
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 9916",
+        "monto": 1900
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35899",
+        "monto": 1000
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35898",
+        "monto": 1650
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35897",
+        "monto": 2000
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35896",
+        "monto": 5900
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 6460",
+        "monto": 2150
+      },
+      {
+        "descripcion": "Venta efectivo",
+        "doc": "Boleta Afecta #35895",
+        "monto": 6650
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 3882",
+        "monto": 5150
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 1985",
+        "monto": 4900
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 4320",
+        "monto": 2400
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 6817",
+        "monto": 16680
+      },
+      {
+        "descripcion": "Venta débito",
+        "doc": "**** 1762",
+        "monto": 1650
+      }
+    ]
   },
   "ventas_por_hora": []
 };
