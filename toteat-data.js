b59e64f4b64a44bdbc43802fb0d1aeb88c23de4b
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-07-05 23:19:35 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,21 +2,62 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "27/06/2026",
-    "fecha_iso": "2026-06-27",
-    "turno": "1494747985014084-00001-20260627TB.001",
+    "fecha": "04/07/2026",
+    "fecha_iso": "2026-07-04",
+    "turno": "1494747985014084-00001-20260704TB.002",
     "apertura": "",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 27670.0,
+    "total_neto": 23254.0,
+    "total_iva": 4416.0,
+    "n_ordenes": 2,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "tarjeta": 27670.0,
+    "productos_top": [
+      {
+        "nombre": "Manzo",
+        "cantidad": 1,
+        "total": 8990.0
+      },
+      {
+        "nombre": "Prosciutto Cotto",
+        "cantidad": 1,
+        "total": 7790.0
+      },
+      {
+        "nombre": "Té variedades",
+        "cantidad": 4,
+        "total": 6400.0
+      },
+      {
+        "nombre": "Completo Italiano",
+        "cantidad": 1,
+        "total": 4490.0
+      }
+    ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-07-04",
+      "turno": "1494747985014084-00001-20260704TB.002"
+    },
+    {
+      "fecha": "2026-07-03",
+      "turno": "1494747985014084-00001-20260703TB.002"
+    },
+    {
+      "fecha": "2026-07-02",
+      "turno": "1494747985014084-00001-20260702TB.001"
+    },
+    {
+      "fecha": "2026-07-01",
+      "turno": "1494747985014084-00001-20260701TB.001"
+    },
+    {
+      "fecha": "2026-06-30",
+      "turno": "1494747985014084-00001-20260630TB.001"
+    },
     {
       "fecha": "2026-06-27",
       "turno": "1494747985014084-00001-20260627TB.001"
@@ -36,24 +77,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-06-23",
       "turno": "1494747985014084-00001-20260623TB.002"
-    },
-    {
-      "fecha": "2026-06-22",
-      "turno": "1494747985014084-00001-20260622TB.001"
-    },
-    {
-      "fecha": "2026-06-20",
-      "turno": "1494747985014084-00001-20260620TB.002"
-    },
-    {
-      "fecha": "2026-06-19",
-      "turno": "1494747985014084-00001-20260619TB.001"
-    },
-    {
-      "fecha": "2026-06-18",
-      "turno": "1494747985014084-00001-20260618TB.002"
     }
   ],
-  "total_cierres_historicos": 98,
-  "actualizadoEl": "29/06/2026 10:09"
+  "total_cierres_historicos": 105,
+  "actualizadoEl": "05/07/2026 22:52"
 };
