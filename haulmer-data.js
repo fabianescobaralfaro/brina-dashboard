0a5dfac1978461948e3a47215c629a8e69b579f4
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-07-16 14:29:02 -0400

    Actualizar haulmer-data.js

--- haulmer-data.js
+++ haulmer-data.js
@@ -1,12 +1,96 @@
 /* Generado automáticamente por scraper.py — NO editar manualmente */
 window.HAULMER_DATA = {
-  "actualizadoEl": "14/07/2026 09:28",
-  "error": "Page.goto: net::ERR_INTERNET_DISCONNECTED at https://accounts.haulmer.com/realms/haulmer-users/protocol/openid-connect/auth?client_id=workspace-prod&redirect_uri=https%3A%2F%2Fespacio.haulmer.com%2F&response_mode=fragment&response_type=code&scope=openid\nCall log:\n  - navigating to \"https://accounts.haulmer.com/realms/haulmer-users/protocol/openid-connect/auth?client_id=workspace-prod&redirect_uri=https%3A%2F%2Fespacio.haulmer.com%2F&response_mode=fragment&response_type=code&scope=openid\", waiting until \"domcontentloaded\"\n",
-  "informe_dia": {},
-  "informe_semana": {},
-  "informe_mes": {},
-  "dte": {},
-  "abonos": {},
-  "ventas_hoy": {},
+  "actualizadoEl": "16/07/2026 12:33",
+  "error": null,
+  "informe_dia": {
+    "periodo": "Día",
+    "monto_neto": 0,
+    "iva": 0,
+    "monto_ventas": 0,
+    "monto_exento": 0,
+    "propinas": 0,
+    "debito": 0,
+    "credito": 0,
+    "efectivo": 0,
+    "pos_breakdown": [],
+    "n_tx_debito": 0,
+    "n_tx_credito": 0,
+    "n_tx_efectivo": 0,
+    "n_tx_total": 0
+  },
+  "informe_semana": {
+    "periodo": "Semana",
+    "monto_neto": 0,
+    "iva": 0,
+    "monto_ventas": 0,
+    "monto_exento": 0,
+    "propinas": 0,
+    "debito": 0,
+    "credito": 0,
+    "efectivo": 0,
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 0,
+        "debito": 0,
+        "credito": 0,
+        "efectivo": 0,
+        "propinas": 0
+      }
+    ],
+    "n_tx_debito": 0,
+    "n_tx_credito": 0,
+    "n_tx_efectivo": 0,
+    "n_tx_total": 0
+  },
+  "informe_mes": {
+    "periodo": "Mes",
+    "monto_neto": 0,
+    "iva": 0,
+    "monto_ventas": 0,
+    "monto_exento": 0,
+    "propinas": 0,
+    "debito": 0,
+    "credito": 0,
+    "efectivo": 0,
+    "pos_breakdown": [
+      {
+        "serial": "PB1T238420182",
+        "total_ventas": 0,
+        "debito": 0,
+        "credito": 0,
+        "efectivo": 0,
+        "propinas": 0
+      }
+    ],
+    "n_tx_debito": 0,
+    "n_tx_credito": 0,
+    "n_tx_efectivo": 0,
+    "n_tx_total": 0
+  },
+  "dte": {
+    "total_declarado": 0,
+    "total": 0,
+    "boletas": 0,
+    "facturas": 0,
+    "monto_boletas": 0,
+    "monto_facturas": 0,
+    "documentos": [],
+    "paginas_leidas": 1,
+    "nota": ""
+  },
+  "abonos": {
+    "abonos": [],
+    "total_abonado": 0,
+    "total_comision": 0,
+    "comision_pct": 1.99,
+    "transacciones_hoy": [],
+    "total_ventas_hoy": 0,
+    "n_transacciones_hoy": 0
+  },
+  "ventas_hoy": {
+    "total_dia": 0,
+    "transacciones": []
+  },
   "ventas_por_hora": []
 };
