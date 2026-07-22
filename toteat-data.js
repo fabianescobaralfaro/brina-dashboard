ff5f9e5c01da07c76f44cad7dc7bd57f77dfb9f2
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-07-21 23:49:12 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,72 +2,88 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "15/07/2026",
-    "fecha_iso": "2026-07-15",
-    "turno": "1494747985014084-00001-20260715TB.002",
-    "apertura": "",
+    "fecha": "21/07/2026",
+    "fecha_iso": "2026-07-21",
+    "turno": "1494747985014084-00001-20260721TB.001",
+    "apertura": "2026-07-21T15:03:30",
     "cierre": "",
-    "total_ventas": 89834.0,
-    "total_neto": 79389.0,
-    "total_iva": 14340.0,
+    "total_ventas": 110440.0,
+    "total_neto": 187597.0,
+    "total_iva": 35643.0,
     "n_ordenes": 8,
     "n_anuladas": 0,
-    "efectivo": 13860.0,
-    "tarjeta": 80469.0,
+    "efectivo": 0,
+    "tarjeta": 119325.0,
     "productos_top": [
       {
-        "nombre": "Churrasco palta tomate mayo",
-        "cantidad": 2,
-        "total": 17980.0
-      },
-      {
-        "nombre": "Camarón",
-        "cantidad": 1,
-        "total": 11990.0
-      },
-      {
-        "nombre": "Pepperoni",
-        "cantidad": 1,
-        "total": 11190.0
-      },
-      {
-        "nombre": "Tradición de la casa",
-        "cantidad": 2,
-        "total": 11000.0
-      },
-      {
-        "nombre": "Manzo",
-        "cantidad": 1,
-        "total": 8990.0
-      },
-      {
-        "nombre": "Promo completo italiano",
-        "cantidad": 3,
-        "total": 8970.0
-      },
-      {
-        "nombre": "Amanecer verde",
-        "cantidad": 1,
-        "total": 7870.0
-      },
-      {
         "nombre": "Menú ejecutivo",
-        "cantidad": 1,
-        "total": 7000.0
+        "cantidad": 7,
+        "total": 49000.0
       },
       {
-        "nombre": "Pastel y café",
+        "nombre": "Ciabatta con huevo",
+        "cantidad": 3,
+        "total": 16500.0
+      },
+      {
+        "nombre": "Mechada",
+        "cantidad": 1,
+        "total": 7990.0
+      },
+      {
+        "nombre": "Pollo",
         "cantidad": 1,
         "total": 5990.0
       },
       {
-        "nombre": "Bebida 350 cc",
+        "nombre": "Fettuccini tradizionale",
         "cantidad": 1,
-        "total": 1890.0
+        "total": 5990.0
+      },
+      {
+        "nombre": "Americano",
+        "cantidad": 3,
+        "total": 5700.0
+      },
+      {
+        "nombre": "Gnocchi con salsa",
+        "cantidad": 1,
+        "total": 4990.0
+      },
+      {
+        "nombre": "Té variedades",
+        "cantidad": 3,
+        "total": 4800.0
+      },
+      {
+        "nombre": "Schop Brodir",
+        "cantidad": 1,
+        "total": 4000.0
+      },
+      {
+        "nombre": "Promo completo italiano",
+        "cantidad": 1,
+        "total": 2990.0
       }
     ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-07-21",
+      "turno": "1494747985014084-00001-20260721TB.001"
+    },
+    {
+      "fecha": "2026-07-20",
+      "turno": "1494747985014084-00001-20260720TB.001"
+    },
+    {
+      "fecha": "2026-07-18",
+      "turno": "1494747985014084-00001-20260718TB.002"
+    },
+    {
+      "fecha": "2026-07-17",
+      "turno": "1494747985014084-00001-20260717TB.001"
+    },
     {
       "fecha": "2026-07-15",
       "turno": "1494747985014084-00001-20260715TB.002"
@@ -87,20 +103,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-07-10",
       "turno": "1494747985014084-00001-20260710TB.002"
-    },
-    {
-      "fecha": "2026-07-09",
-      "turno": "1494747985014084-00001-20260709TB.002"
-    },
-    {
-      "fecha": "2026-07-08",
-      "turno": "1494747985014084-00001-20260708TB.002"
-    },
-    {
-      "fecha": "2026-07-07",
-      "turno": "1494747985014084-00001-20260707TB.002"
     }
   ],
-  "total_cierres_historicos": 121,
-  "actualizadoEl": "16/07/2026 21:31"
+  "total_cierres_historicos": 126,
+  "actualizadoEl": "21/07/2026 23:37"
 };
