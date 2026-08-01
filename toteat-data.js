df0fe4f5184424230d3aead392caf98ff43caee8
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-07-31 23:27:13 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,109 +2,91 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "21/07/2026",
-    "fecha_iso": "2026-07-21",
-    "turno": "1494747985014084-00001-20260721TB.001",
-    "apertura": "2026-07-21T15:03:30",
+    "fecha": "31/07/2026",
+    "fecha_iso": "2026-07-31",
+    "turno": "1494747985014084-00001-20260731TB.002",
+    "apertura": "",
     "cierre": "",
-    "total_ventas": 110440.0,
-    "total_neto": 187597.0,
-    "total_iva": 35643.0,
+    "total_ventas": 81510.0,
+    "total_neto": 125639.0,
+    "total_iva": 23871.0,
     "n_ordenes": 8,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 119325.0,
+    "tarjeta": 89661.0,
     "productos_top": [
       {
         "nombre": "Menú ejecutivo",
-        "cantidad": 7,
-        "total": 49000.0
+        "cantidad": 6,
+        "total": 42000.0
       },
       {
-        "nombre": "Ciabatta con huevo",
+        "nombre": "Promo completo italiano",
         "cantidad": 3,
-        "total": 16500.0
+        "total": 8970.0
       },
       {
-        "nombre": "Mechada",
-        "cantidad": 1,
-        "total": 7990.0
+        "nombre": "Capuccino",
+        "cantidad": 3,
+        "total": 7470.0
       },
       {
-        "nombre": "Pollo",
+        "nombre": "Papas fritas",
+        "cantidad": 3,
+        "total": 6000.0
+      },
+      {
+        "nombre": "As italiano",
         "cantidad": 1,
         "total": 5990.0
       },
       {
-        "nombre": "Fettuccini tradizionale",
-        "cantidad": 1,
-        "total": 5990.0
-      },
-      {
-        "nombre": "Americano",
-        "cantidad": 3,
-        "total": 5700.0
-      },
-      {
-        "nombre": "Gnocchi con salsa",
+        "nombre": "Fettuccini con salsa",
         "cantidad": 1,
         "total": 4990.0
       },
       {
+        "nombre": "Completo Italiano",
+        "cantidad": 1,
+        "total": 4490.0
+      },
+      {
         "nombre": "Té variedades",
-        "cantidad": 3,
-        "total": 4800.0
-      },
-      {
-        "nombre": "Schop Brodir",
         "cantidad": 1,
-        "total": 4000.0
-      },
-      {
-        "nombre": "Promo completo italiano",
-        "cantidad": 1,
-        "total": 2990.0
+        "total": 1600.0
       }
     ]
   },
   "historial_cierres": [
     {
-      "fecha": "2026-07-21",
-      "turno": "1494747985014084-00001-20260721TB.001"
+      "fecha": "2026-07-31",
+      "turno": "1494747985014084-00001-20260731TB.002"
     },
     {
-      "fecha": "2026-07-20",
-      "turno": "1494747985014084-00001-20260720TB.001"
+      "fecha": "2026-07-30",
+      "turno": "1494747985014084-00001-20260730TB.002"
     },
     {
-      "fecha": "2026-07-18",
-      "turno": "1494747985014084-00001-20260718TB.002"
+      "fecha": "2026-07-28",
+      "turno": "1494747985014084-00001-20260728TB.002"
     },
     {
-      "fecha": "2026-07-17",
-      "turno": "1494747985014084-00001-20260717TB.001"
+      "fecha": "2026-07-27",
+      "turno": "1494747985014084-00001-20260727TB.002"
     },
     {
-      "fecha": "2026-07-15",
-      "turno": "1494747985014084-00001-20260715TB.002"
+      "fecha": "2026-07-25",
+      "turno": "1494747985014084-00001-20260725TB.002"
     },
     {
-      "fecha": "2026-07-14",
-      "turno": "1494747985014084-00001-20260714TB.002"
+      "fecha": "2026-07-24",
+      "turno": "1494747985014084-00001-20260724TB.002"
     },
     {
-      "fecha": "2026-07-13",
-      "turno": "1494747985014084-00001-20260713TB.001"
-    },
-    {
-      "fecha": "2026-07-11",
-      "turno": "1494747985014084-00001-20260711TB.002"
-    },
-    {
-      "fecha": "2026-07-10",
-      "turno": "1494747985014084-00001-20260710TB.002"
+      "fecha": "2026-07-23",
+      "turno": "1494747985014084-00001-20260723TB.002"
     }
   ],
-  "total_cierres_historicos": 126,
-  "actualizadoEl": "22/07/2026 00:01"
+  "total_cierres_historicos": 142,
+  "actualizadoEl": "31/07/2026 23:13"
 };
