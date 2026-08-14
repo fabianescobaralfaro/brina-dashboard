17d31fef543b4fc2a86af337101fdd2e2a6917ed
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-08-14 12:27:34 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,91 +2,69 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "31/07/2026",
-    "fecha_iso": "2026-07-31",
-    "turno": "1494747985014084-00001-20260731TB.002",
-    "apertura": "",
+    "fecha": "14/08/2026",
+    "fecha_iso": "2026-08-14",
+    "turno": "1494747985014084-00001-20260814TB.001",
+    "apertura": "2026-08-14T12:54:30",
     "cierre": "",
-    "total_ventas": 81510.0,
-    "total_neto": 125639.0,
-    "total_iva": 23871.0,
-    "n_ordenes": 8,
+    "total_ventas": 17300.0,
+    "total_neto": 14538.0,
+    "total_iva": 2762.0,
+    "n_ordenes": 2,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 89661.0,
+    "tarjeta": 19030.0,
     "productos_top": [
       {
-        "nombre": "Menú ejecutivo",
-        "cantidad": 6,
-        "total": 42000.0
+        "nombre": "Amanecer verde",
+        "cantidad": 2,
+        "total": 11800.0
       },
       {
-        "nombre": "Promo completo italiano",
-        "cantidad": 3,
-        "total": 8970.0
-      },
-      {
-        "nombre": "Capuccino",
-        "cantidad": 3,
-        "total": 7470.0
-      },
-      {
-        "nombre": "Papas fritas",
-        "cantidad": 3,
-        "total": 6000.0
-      },
-      {
-        "nombre": "As italiano",
+        "nombre": "Ciabatta con huevo",
         "cantidad": 1,
-        "total": 5990.0
-      },
-      {
-        "nombre": "Fettuccini con salsa",
-        "cantidad": 1,
-        "total": 4990.0
-      },
-      {
-        "nombre": "Completo Italiano",
-        "cantidad": 1,
-        "total": 4490.0
-      },
-      {
-        "nombre": "Té variedades",
-        "cantidad": 1,
-        "total": 1600.0
+        "total": 5500.0
       }
     ]
   },
   "historial_cierres": [
     {
-      "fecha": "2026-07-31",
-      "turno": "1494747985014084-00001-20260731TB.002"
+      "fecha": "2026-08-14",
+      "turno": "1494747985014084-00001-20260814TB.001"
     },
     {
-      "fecha": "2026-07-30",
-      "turno": "1494747985014084-00001-20260730TB.002"
+      "fecha": "2026-08-13",
+      "turno": "1494747985014084-00001-20260813TB.002"
     },
     {
-      "fecha": "2026-07-28",
-      "turno": "1494747985014084-00001-20260728TB.002"
+      "fecha": "2026-08-12",
+      "turno": "1494747985014084-00001-20260812TB.002"
     },
     {
-      "fecha": "2026-07-27",
-      "turno": "1494747985014084-00001-20260727TB.002"
+      "fecha": "2026-08-11",
+      "turno": "1494747985014084-00001-20260811TB.001"
     },
     {
-      "fecha": "2026-07-25",
-      "turno": "1494747985014084-00001-20260725TB.002"
+      "fecha": "2026-08-10",
+      "turno": "1494747985014084-00001-20260810TB.002"
     },
     {
-      "fecha": "2026-07-24",
-      "turno": "1494747985014084-00001-20260724TB.002"
+      "fecha": "2026-08-08",
+      "turno": "1494747985014084-00001-20260808TB.001"
     },
     {
-      "fecha": "2026-07-23",
-      "turno": "1494747985014084-00001-20260723TB.002"
+      "fecha": "2026-08-07",
+      "turno": "1494747985014084-00001-20260807TB.002"
+    },
+    {
+      "fecha": "2026-08-06",
+      "turno": "1494747985014084-00001-20260806TB.002"
+    },
+    {
+      "fecha": "2026-08-05",
+      "turno": "1494747985014084-00001-20260805TB.002"
     }
   ],
-  "total_cierres_historicos": 142,
-  "actualizadoEl": "31/07/2026 23:13"
+  "total_cierres_historicos": 161,
+  "actualizadoEl": "14/08/2026 12:08"
 };
