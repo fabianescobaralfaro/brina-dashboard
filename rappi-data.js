e9e2f21948c4eefff81365866d1d23f7c2239ce2
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-08-22 18:08:04 -0400

    Actualizar rappi-data.js

--- rappi-data.js
+++ rappi-data.js
@@ -5,49 +5,49 @@ window.RAPPI_DATA = {
   "is_enabled": true,
   "store_tier": "standard",
   "ventas_7d": {
-    "total_amount": 8130.0,
-    "total_orders": 1,
-    "orders_avg": 8130.0,
+    "total_amount": 0,
+    "total_orders": 0,
+    "orders_avg": 0,
     "users_count": 0,
     "new_users_count": 0,
     "last_week": [
       {
-        "date": "2026-08-07",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-08",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-09",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-10",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-11",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-12",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-13",
-        "amount": 8130.0,
-        "orders": 1
-      },
-      {
         "date": "2026-08-14",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-15",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-16",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-17",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-18",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-19",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-20",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-08-21",
         "amount": 0,
         "orders": 0
       }
@@ -59,5 +59,5 @@ window.RAPPI_DATA = {
     "availability_pct": 100.0,
     "cooking_time_avg": 0
   },
-  "actualizadoEl": "15/08/2026 00:03"
+  "actualizadoEl": "22/08/2026 08:20"
 };
