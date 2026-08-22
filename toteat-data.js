f83e833eb60f695e718595056a2b4a9e989b9eb5
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-08-22 18:08:00 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,32 +2,41 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "14/08/2026",
-    "fecha_iso": "2026-08-14",
-    "turno": "1494747985014084-00001-20260814TB.001",
-    "apertura": "2026-08-14T12:54:30",
+    "fecha": "21/08/2026",
+    "fecha_iso": "2026-08-21",
+    "turno": "1494747985014084-00001-20260821TB.002",
+    "apertura": "",
     "cierre": "",
-    "total_ventas": 17300.0,
-    "total_neto": 14538.0,
-    "total_iva": 2762.0,
-    "n_ordenes": 2,
+    "total_ventas": 0,
+    "total_neto": 0,
+    "total_iva": 0,
+    "n_ordenes": 0,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 19030.0,
-    "productos_top": [
-      {
-        "nombre": "Amanecer verde",
-        "cantidad": 2,
-        "total": 11800.0
-      },
-      {
-        "nombre": "Ciabatta con huevo",
-        "cantidad": 1,
-        "total": 5500.0
-      }
-    ]
+    "tarjeta": 0,
+    "productos_top": []
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-08-21",
+      "turno": "1494747985014084-00001-20260821TB.002"
+    },
+    {
+      "fecha": "2026-08-20",
+      "turno": "1494747985014084-00001-20260820TB.001"
+    },
+    {
+      "fecha": "2026-08-19",
+      "turno": "1494747985014084-00001-20260819TB.001"
+    },
+    {
+      "fecha": "2026-08-18",
+      "turno": "1494747985014084-00001-20260818TB.002"
+    },
+    {
+      "fecha": "2026-08-17",
+      "turno": "1494747985014084-00001-20260817TB.001"
+    },
     {
       "fecha": "2026-08-14",
       "turno": "1494747985014084-00001-20260814TB.001"
@@ -47,24 +56,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-08-10",
       "turno": "1494747985014084-00001-20260810TB.002"
-    },
-    {
-      "fecha": "2026-08-08",
-      "turno": "1494747985014084-00001-20260808TB.001"
-    },
-    {
-      "fecha": "2026-08-07",
-      "turno": "1494747985014084-00001-20260807TB.002"
-    },
-    {
-      "fecha": "2026-08-06",
-      "turno": "1494747985014084-00001-20260806TB.002"
-    },
-    {
-      "fecha": "2026-08-05",
-      "turno": "1494747985014084-00001-20260805TB.002"
     }
   ],
-  "total_cierres_historicos": 161,
-  "actualizadoEl": "14/08/2026 12:08"
+  "total_cierres_historicos": 168,
+  "actualizadoEl": "22/08/2026 08:19"
 };
