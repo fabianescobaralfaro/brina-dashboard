ff0385138c417c79f53b16fb4e1662ad5b6f2881
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-08-23 12:26:34 -0400

    Actualizar haulmer-data.js

--- haulmer-data.js
+++ haulmer-data.js
@@ -1,6 +1,6 @@
 /* Generado automáticamente por scraper.py — NO editar manualmente */
 window.HAULMER_DATA = {
-  "actualizadoEl": "22/08/2026 08:18",
+  "actualizadoEl": "23/08/2026 12:20",
   "error": null,
   "informe_dia": {
     "periodo": "Día",
@@ -29,53 +29,69 @@ window.HAULMER_DATA = {
   },
   "informe_semana": {
     "periodo": "Semana",
-    "monto_neto": 0,
-    "iva": 0,
-    "monto_ventas": 0,
-    "monto_exento": 0,
-    "propinas": 0,
-    "debito": 0,
-    "credito": 0,
-    "efectivo": 0,
+    "monto_neto": 1668700,
+    "iva": 317080,
+    "monto_ventas": 1985780,
+    "monto_exento": 1985780,
+    "propinas": 79805,
+    "debito": 1343463,
+    "credito": 146656,
+    "efectivo": 575470,
     "pos_breakdown": [
       {
         "serial": "PB1T238420182",
-        "total_ventas": 0,
-        "debito": 0,
-        "credito": 0,
-        "efectivo": 0,
+        "total_ventas": 1137250,
+        "debito": 608420,
+        "credito": 57900,
+        "efectivo": 470930,
+        "propinas": 0
+      },
+      {
+        "serial": "6010B232511900275",
+        "total_ventas": 928339,
+        "debito": 735043,
+        "credito": 88756,
+        "efectivo": 104540,
         "propinas": 0
       }
     ],
-    "n_tx_debito": 0,
-    "n_tx_credito": 0,
-    "n_tx_efectivo": 0,
-    "n_tx_total": 0
+    "n_tx_debito": 297,
+    "n_tx_credito": 32,
+    "n_tx_efectivo": 199,
+    "n_tx_total": 528
   },
   "informe_mes": {
     "periodo": "Mes",
-    "monto_neto": 0,
-    "iva": 0,
-    "monto_ventas": 0,
-    "monto_exento": 0,
-    "propinas": 0,
-    "debito": 0,
-    "credito": 0,
-    "efectivo": 0,
+    "monto_neto": 5237724,
+    "iva": 995260,
+    "monto_ventas": 6232984,
+    "monto_exento": 6232984,
+    "propinas": 248147,
+    "debito": 4358852,
+    "credito": 392401,
+    "efectivo": 1729890,
     "pos_breakdown": [
       {
         "serial": "PB1T238420182",
-        "total_ventas": 0,
-        "debito": 0,
-        "credito": 0,
-        "efectivo": 0,
+        "total_ventas": 3497570,
+        "debito": 1953470,
+        "credito": 184650,
+        "efectivo": 1359450,
+        "propinas": 0
+      },
+      {
+        "serial": "6010B232511900275",
+        "total_ventas": 2983573,
+        "debito": 2405382,
+        "credito": 207751,
+        "efectivo": 370440,
         "propinas": 0
       }
     ],
-    "n_tx_debito": 0,
-    "n_tx_credito": 0,
-    "n_tx_efectivo": 0,
-    "n_tx_total": 0
+    "n_tx_debito": 957,
+    "n_tx_credito": 98,
+    "n_tx_efectivo": 663,
+    "n_tx_total": 1718
   },
   "dte": {
     "total_declarado": 0,
