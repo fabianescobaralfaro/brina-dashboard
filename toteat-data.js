e815708d107e551ebbe779f67c3baa587d6cc340
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-01 10:13:51 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,52 +2,37 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "27/08/2026",
-    "fecha_iso": "2026-08-27",
-    "turno": "1494747985014084-00001-20260827TB.001",
-    "apertura": "",
+    "fecha": "01/09/2026",
+    "fecha_iso": "2026-09-01",
+    "turno": "1494747985014084-00001-20260901TB.001",
+    "apertura": "2026-09-01T13:03:35",
     "cierre": "",
-    "total_ventas": 57760.0,
-    "total_neto": 67681.0,
-    "total_iva": 12859.0,
-    "n_ordenes": 5,
+    "total_ventas": 0,
+    "total_neto": 0,
+    "total_iva": 0,
+    "n_ordenes": 0,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 63536.0,
-    "productos_top": [
-      {
-        "nombre": "Menú ejecutivo",
-        "cantidad": 4,
-        "total": 28000.0
-      },
-      {
-        "nombre": "Menú premium",
-        "cantidad": 1,
-        "total": 9000.0
-      },
-      {
-        "nombre": "Manzo",
-        "cantidad": 1,
-        "total": 8990.0
-      },
-      {
-        "nombre": "Papas fritas",
-        "cantidad": 2,
-        "total": 5000.0
-      },
-      {
-        "nombre": "Bebida 350 cc",
-        "cantidad": 2,
-        "total": 3780.0
-      },
-      {
-        "nombre": "Promo completo italiano",
-        "cantidad": 1,
-        "total": 2990.0
-      }
-    ]
+    "tarjeta": 0,
+    "productos_top": []
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-09-01",
+      "turno": "1494747985014084-00001-20260901TB.001"
+    },
+    {
+      "fecha": "2026-08-31",
+      "turno": "1494747985014084-00001-20260831TB.001"
+    },
+    {
+      "fecha": "2026-08-29",
+      "turno": "1494747985014084-00001-20260829TB.001"
+    },
+    {
+      "fecha": "2026-08-28",
+      "turno": "1494747985014084-00001-20260828TB.002"
+    },
     {
       "fecha": "2026-08-27",
       "turno": "1494747985014084-00001-20260827TB.001"
@@ -71,24 +56,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-08-21",
       "turno": "1494747985014084-00001-20260821TB.002"
-    },
-    {
-      "fecha": "2026-08-20",
-      "turno": "1494747985014084-00001-20260820TB.001"
-    },
-    {
-      "fecha": "2026-08-19",
-      "turno": "1494747985014084-00001-20260819TB.001"
-    },
-    {
-      "fecha": "2026-08-18",
-      "turno": "1494747985014084-00001-20260818TB.002"
-    },
-    {
-      "fecha": "2026-08-17",
-      "turno": "1494747985014084-00001-20260817TB.001"
     }
   ],
-  "total_cierres_historicos": 175,
-  "actualizadoEl": "28/08/2026 07:39"
+  "total_cierres_historicos": 180,
+  "actualizadoEl": "01/09/2026 09:13"
 };
