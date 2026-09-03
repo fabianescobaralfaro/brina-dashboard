5f54301eb6caf87d7f2bb15a2c0fa97c82e72ad3
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-03 15:28:32 -0400

    Actualizar rappi-data.js

--- rappi-data.js
+++ rappi-data.js
@@ -2,7 +2,7 @@
 window.RAPPI_DATA = {
   "error": null,
   "store_name": "Brina Pizzeria",
-  "is_enabled": true,
+  "is_enabled": false,
   "store_tier": "standard",
   "ventas_7d": {
     "total_amount": 0,
@@ -10,54 +10,13 @@ window.RAPPI_DATA = {
     "orders_avg": 0,
     "users_count": 0,
     "new_users_count": 0,
-    "last_week": [
-      {
-        "date": "2026-08-24",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-25",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-26",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-27",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-28",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-29",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-30",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-31",
-        "amount": 0,
-        "orders": 0
-      }
-    ]
+    "last_week": []
   },
   "indicadores": {
-    "cancellation_pct": 100.0,
+    "cancellation_pct": 0,
     "error_pct": 0,
-    "availability_pct": 100.0,
+    "availability_pct": 0,
     "cooking_time_avg": 0
   },
-  "actualizadoEl": "01/09/2026 09:14"
+  "actualizadoEl": "03/09/2026 13:02"
 };
