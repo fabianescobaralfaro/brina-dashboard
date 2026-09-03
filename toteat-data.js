e989e65c0b9d932b8ae1d7cb8759ea779ea4ce67
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-03 15:28:29 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,24 +2,43 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "01/09/2026",
-    "fecha_iso": "2026-09-01",
-    "turno": "1494747985014084-00001-20260901TB.001",
-    "apertura": "2026-09-01T13:03:35",
+    "fecha": "03/09/2026",
+    "fecha_iso": "2026-09-03",
+    "turno": "1494747985014084-00001-20260903TB.001",
+    "apertura": "2026-09-03T11:51:44",
     "cierre": "",
-    "total_ventas": 0,
-    "total_neto": 0,
-    "total_iva": 0,
-    "n_ordenes": 0,
+    "total_ventas": 14490.0,
+    "total_neto": 12177.0,
+    "total_iva": 2313.0,
+    "n_ordenes": 1,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 0,
-    "productos_top": []
+    "tarjeta": 15939.0,
+    "productos_top": [
+      {
+        "nombre": "Lomito chacarero",
+        "cantidad": 1,
+        "total": 8990.0
+      },
+      {
+        "nombre": "Tradición de la casa",
+        "cantidad": 1,
+        "total": 5500.0
+      }
+    ]
   },
   "historial_cierres": [
     {
+      "fecha": "2026-09-03",
+      "turno": "1494747985014084-00001-20260903TB.001"
+    },
+    {
+      "fecha": "2026-09-02",
+      "turno": "1494747985014084-00001-20260902TB.002"
+    },
+    {
       "fecha": "2026-09-01",
-      "turno": "1494747985014084-00001-20260901TB.001"
+      "turno": "1494747985014084-00001-20260901TB.002"
     },
     {
       "fecha": "2026-08-31",
@@ -44,20 +63,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-08-25",
       "turno": "1494747985014084-00001-20260825TB.002"
-    },
-    {
-      "fecha": "2026-08-24",
-      "turno": "1494747985014084-00001-20260824TB.001"
-    },
-    {
-      "fecha": "2026-08-22",
-      "turno": "1494747985014084-00001-20260822TB.001"
-    },
-    {
-      "fecha": "2026-08-21",
-      "turno": "1494747985014084-00001-20260821TB.002"
     }
   ],
-  "total_cierres_historicos": 180,
-  "actualizadoEl": "01/09/2026 09:13"
+  "total_cierres_historicos": 184,
+  "actualizadoEl": "03/09/2026 14:51"
 };
