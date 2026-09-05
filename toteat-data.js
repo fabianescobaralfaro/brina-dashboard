7c2e85fa389122f07e2f06fd6f12b5093b96062e
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-05 18:59:36 -0400

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,35 +2,83 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "03/09/2026",
-    "fecha_iso": "2026-09-03",
-    "turno": "1494747985014084-00001-20260903TB.001",
-    "apertura": "2026-09-03T11:51:44",
+    "fecha": "05/09/2026",
+    "fecha_iso": "2026-09-05",
+    "turno": "1494747985014084-00001-20260905TB.001",
+    "apertura": "",
     "cierre": "",
-    "total_ventas": 14490.0,
-    "total_neto": 12177.0,
-    "total_iva": 2313.0,
-    "n_ordenes": 1,
+    "total_ventas": 113150.0,
+    "total_neto": 119425.0,
+    "total_iva": 22151.0,
+    "n_ordenes": 6,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 15939.0,
+    "tarjeta": 123669.0,
     "productos_top": [
       {
-        "nombre": "Lomito chacarero",
-        "cantidad": 1,
-        "total": 8990.0
+        "nombre": "Mechada queso y palta",
+        "cantidad": 2,
+        "total": 20120.0
       },
       {
-        "nombre": "Tradición de la casa",
+        "nombre": "Mechada palta, tomate y mayo",
+        "cantidad": 2,
+        "total": 18000.0
+      },
+      {
+        "nombre": "Pepperoni",
+        "cantidad": 2,
+        "total": 16780.0
+      },
+      {
+        "nombre": "Menú ejecutivo",
+        "cantidad": 2,
+        "total": 14000.0
+      },
+      {
+        "nombre": "Menú premium",
         "cantidad": 1,
-        "total": 5500.0
+        "total": 9000.0
+      },
+      {
+        "nombre": "Prosciutto Cotto",
+        "cantidad": 1,
+        "total": 7790.0
+      },
+      {
+        "nombre": "Pollo Barbecue",
+        "cantidad": 1,
+        "total": 7190.0
+      },
+      {
+        "nombre": "As italiano",
+        "cantidad": 1,
+        "total": 5990.0
+      },
+      {
+        "nombre": "Bebida 350 cc",
+        "cantidad": 3,
+        "total": 5670.0
+      },
+      {
+        "nombre": "1/4 pizza y bebida",
+        "cantidad": 1,
+        "total": 3950.0
       }
     ]
   },
   "historial_cierres": [
     {
+      "fecha": "2026-09-05",
+      "turno": "1494747985014084-00001-20260905TB.001"
+    },
+    {
+      "fecha": "2026-09-04",
+      "turno": "1494747985014084-00001-20260904TB.001"
+    },
+    {
       "fecha": "2026-09-03",
-      "turno": "1494747985014084-00001-20260903TB.001"
+      "turno": "1494747985014084-00001-20260903TB.002"
     },
     {
       "fecha": "2026-09-02",
@@ -59,12 +107,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-08-26",
       "turno": "1494747985014084-00001-20260826TB.002"
-    },
-    {
-      "fecha": "2026-08-25",
-      "turno": "1494747985014084-00001-20260825TB.002"
     }
   ],
-  "total_cierres_historicos": 184,
-  "actualizadoEl": "03/09/2026 14:51"
+  "total_cierres_historicos": 187,
+  "actualizadoEl": "05/09/2026 18:50"
 };
