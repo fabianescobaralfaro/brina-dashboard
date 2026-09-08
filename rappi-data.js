bc99d6ec6c9bfc71fe9c017dab68590b5c6f1706
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-08 19:43:14 -0300

    Actualizar rappi-data.js

--- rappi-data.js
+++ rappi-data.js
@@ -5,32 +5,12 @@ window.RAPPI_DATA = {
   "is_enabled": true,
   "store_tier": "standard",
   "ventas_7d": {
-    "total_amount": 6490.0,
-    "total_orders": 1,
-    "orders_avg": 6490.0,
-    "users_count": 1,
-    "new_users_count": 1,
+    "total_amount": 28720.0,
+    "total_orders": 3,
+    "orders_avg": 9573.33,
+    "users_count": 3,
+    "new_users_count": 3,
     "last_week": [
-      {
-        "date": "2026-08-27",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-28",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-29",
-        "amount": 0,
-        "orders": 0
-      },
-      {
-        "date": "2026-08-30",
-        "amount": 0,
-        "orders": 0
-      },
       {
         "date": "2026-08-31",
         "amount": 0,
@@ -50,14 +30,34 @@ window.RAPPI_DATA = {
         "date": "2026-09-03",
         "amount": 0,
         "orders": 0
+      },
+      {
+        "date": "2026-09-04",
+        "amount": 14270.0,
+        "orders": 1
+      },
+      {
+        "date": "2026-09-05",
+        "amount": 7960.0,
+        "orders": 1
+      },
+      {
+        "date": "2026-09-06",
+        "amount": 0,
+        "orders": 0
+      },
+      {
+        "date": "2026-09-07",
+        "amount": 0,
+        "orders": 0
       }
     ]
   },
   "indicadores": {
-    "cancellation_pct": 50.0,
-    "error_pct": 0,
+    "cancellation_pct": 0,
+    "error_pct": 100.0,
     "availability_pct": 100.0,
     "cooking_time_avg": 0
   },
-  "actualizadoEl": "04/09/2026 10:40"
+  "actualizadoEl": "08/09/2026 09:02"
 };
