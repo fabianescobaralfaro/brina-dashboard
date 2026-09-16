836163d9c774678a4a56805fd7e1523de7d50ae6
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-15 22:26:38 -0300

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,37 +2,36 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "14/09/2026",
-    "fecha_iso": "2026-09-14",
-    "turno": "1494747985014084-00001-20260914TB.002",
-    "apertura": "",
+    "fecha": "15/09/2026",
+    "fecha_iso": "2026-09-15",
+    "turno": "1494747985014084-00001-20260915TB.002",
+    "apertura": "2026-09-15T16:32:32",
     "cierre": "",
-    "total_ventas": 12490.0,
-    "total_neto": 10496.0,
-    "total_iva": 1994.0,
-    "n_ordenes": 3,
-    "n_anuladas": 1,
-    "efectivo": 3289.0,
-    "tarjeta": 9500.0,
+    "total_ventas": 7960.0,
+    "total_neto": 9555.0,
+    "total_iva": 1271.0,
+    "n_ordenes": 1,
+    "n_anuladas": 0,
+    "efectivo": 0,
+    "tarjeta": 7960.0,
     "productos_top": [
       {
-        "nombre": "Menú ejecutivo",
+        "nombre": "Mechada queso y palta",
         "cantidad": 1,
-        "total": 7000.0
+        "total": 11370.0
       },
       {
-        "nombre": "Promo completo italiano",
+        "nombre": "Coca cola",
         "cantidad": 1,
-        "total": 2990.0
-      },
-      {
-        "nombre": "Completo italiano sólo",
-        "cantidad": 1,
-        "total": 2500.0
+        "total": 0.0
       }
     ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-09-15",
+      "turno": "1494747985014084-00001-20260915TB.002"
+    },
     {
       "fecha": "2026-09-14",
       "turno": "1494747985014084-00001-20260914TB.002"
@@ -60,12 +59,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-09-07",
       "turno": "1494747985014084-00001-20260907TB.001"
-    },
-    {
-      "fecha": "2026-09-05",
-      "turno": "1494747985014084-00001-20260905TB.002"
     }
   ],
-  "total_cierres_historicos": 200,
-  "actualizadoEl": "14/09/2026 21:16"
+  "total_cierres_historicos": 202,
+  "actualizadoEl": "15/09/2026 22:24"
 };
