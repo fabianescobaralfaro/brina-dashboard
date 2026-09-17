e91f6013ac309b6a1e9934aebd24bc6573c45981
fabianescobaralfaro <fabian.escobar.alfaro@gmail.com>
2026-09-16 21:15:45 -0300

    Actualizar toteat-data.js

--- toteat-data.js
+++ toteat-data.js
@@ -2,32 +2,51 @@
 window.TOTEAT_DATA = {
   "error": null,
   "ultimo_cierre": {
-    "fecha": "15/09/2026",
-    "fecha_iso": "2026-09-15",
-    "turno": "1494747985014084-00001-20260915TB.002",
-    "apertura": "2026-09-15T16:32:32",
+    "fecha": "16/09/2026",
+    "fecha_iso": "2026-09-16",
+    "turno": "1494747985014084-00001-20260916TB.002",
+    "apertura": "",
     "cierre": "",
-    "total_ventas": 7960.0,
-    "total_neto": 9555.0,
-    "total_iva": 1271.0,
-    "n_ordenes": 1,
+    "total_ventas": 18360.0,
+    "total_neto": 15426.0,
+    "total_iva": 2934.0,
+    "n_ordenes": 3,
     "n_anuladas": 0,
     "efectivo": 0,
-    "tarjeta": 7960.0,
+    "tarjeta": 19947.0,
     "productos_top": [
       {
-        "nombre": "Mechada queso y palta",
+        "nombre": "Menú ejecutivo",
         "cantidad": 1,
-        "total": 11370.0
+        "total": 7000.0
       },
       {
-        "nombre": "Coca cola",
+        "nombre": "Americano",
+        "cantidad": 2,
+        "total": 4980.0
+      },
+      {
+        "nombre": "Bebida express",
+        "cantidad": 2,
+        "total": 2600.0
+      },
+      {
+        "nombre": "Néctar 300 cc",
         "cantidad": 1,
-        "total": 0.0
+        "total": 1890.0
+      },
+      {
+        "nombre": "Bebida 350 cc",
+        "cantidad": 1,
+        "total": 1890.0
       }
     ]
   },
   "historial_cierres": [
+    {
+      "fecha": "2026-09-16",
+      "turno": "1494747985014084-00001-20260916TB.002"
+    },
     {
       "fecha": "2026-09-15",
       "turno": "1494747985014084-00001-20260915TB.002"
@@ -55,12 +74,8 @@ window.TOTEAT_DATA = {
     {
       "fecha": "2026-09-08",
       "turno": "1494747985014084-00001-20260908TB.002"
-    },
-    {
-      "fecha": "2026-09-07",
-      "turno": "1494747985014084-00001-20260907TB.001"
     }
   ],
-  "total_cierres_historicos": 202,
-  "actualizadoEl": "15/09/2026 22:52"
+  "total_cierres_historicos": 204,
+  "actualizadoEl": "16/09/2026 21:12"
 };
